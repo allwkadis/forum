5bff70895d1696e2534637483059a58b693cd7d2
allwkadis <kadisbinance@gmail.com>
2025-01-15 18:10:06 +0500

    remove unnecessary imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,13 @@
 import { AppRouter } from "./providers/RouterProvider";
-import { useTheme } from "../shared/lib";
 import { classNames } from "../shared/lib";
-
 import "../shared/config/i18n/i18n";
-import { StoreProvider } from "./providers/StoreProvider";
-
-// import "./styles/index.css";
 
 function App() {
   // const { theme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [])}>
-        <AppRouter />
+      <AppRouter />
     </div>
   );
 }
