63659789db590f16a339bae612e8e47f7f3989ae
allwkadis <kadisbinance@gmail.com>
2025-01-23 08:19:48 +0500

    refactoring langSwitcher

--- src/widgets/LangSwitcher/ui/LangSwitcher.tsx
+++ src/widgets/LangSwitcher/ui/LangSwitcher.tsx
@@ -1,36 +1,19 @@
 import { useTranslation } from "react-i18next";
 import { memo } from "react";
 
-import { classNames } from "../../../shared/lib";
-
-import cls from "./LangSwitcher.module.css";
+import { Select } from "../../../shared/ui/Select";
 
 type lang = "en" | "ru";
-interface langOption {
-  lang: lang;
-  id: number;
-}
 
-const lagns: langOption[] = [
-  { lang: "ru", id: 1 },
-  { lang: "en", id: 2 },
+const langs = [
+  { value: "RU", content: "ru" },
+  { value: "en", content: "en" },
 ];
 
 export const LangSwitcher = memo(() => {
   const { i18n } = useTranslation();
 
-  const toogleLang = (e: React.ChangeEvent<HTMLSelectElement>) =>
-    i18n.changeLanguage(e.target.value as lang);
+  const toogleLang = (value: string) => i18n.changeLanguage(value as lang);
 
-  return (
-    <select
-      onChange={toogleLang}
-      defaultValue={i18n.language}
-      className={classNames(cls.LangSwither, {})}
-    >
-      {lagns.map((lang) => {
-        return <option key={lang.id}>{lang.lang}</option>;
-      })}
-    </select>
-  );
+  return <Select options={langs} onChange={toogleLang} />;
 });
