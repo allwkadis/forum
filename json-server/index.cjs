75f6eb04b3b665b96db8f3905df03ae0325dbf09
allwkadis <kadisbinance@gmail.com>
2025-03-24 17:00:50 +0500

    fix

--- json-server/index.cjs
+++ json-server/index.cjs
@@ -12,7 +12,7 @@ const router = jsonServer.router(path.resolve(__dirname, "db.json"));
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
-// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+
 server.use(async (req, res, next) => {
   await new Promise((res) => {
     setTimeout(res, 800);
@@ -20,7 +20,7 @@ server.use(async (req, res, next) => {
   next();
 });
 
-// Эндпоинт для логина
+
 server.post("/login", (req, res) => {
   try {
     const { username, password } = req.body;
@@ -44,8 +44,7 @@ server.post("/login", (req, res) => {
   }
 });
 
-// проверяем, авторизован ли пользователь
-// eslint-disable-next-line
+
 server.use((req, res, next) => {
   if (!req.headers.authorization) {
     return res.status(403).json({ message: "AUTH ERROR" });
@@ -56,7 +55,7 @@ server.use((req, res, next) => {
 
 server.use(router);
 
-// запуск сервера
+
 server.listen(8000, () => {
   console.log("server is running on 8000 port");
 });
